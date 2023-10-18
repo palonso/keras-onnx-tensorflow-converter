7bc4dad7a0220626a1766e26fcf89e6d5c271959
palonso <pablo.alonso@upf.edu>
2023-10-18 17:58:30 +0200

    Add functionallity to select output node

--- src/add_output_node.py
+++ src/add_output_node.py
@@ -18,6 +18,7 @@ def add_output_node(
     node_in=None,
     node_out=None,
     output_shape=None,
+    node_type="identity",
 ):
     model = onnx.load(model_in)
     print(f"Searchoing for node {node_in} in {model_in}")
@@ -29,7 +30,7 @@ def add_output_node(
             print("output node found!")
 
     if not new_output:
-        raise Exception(f"Node {node_in} not found in {model_in}")
+        raise Exception(f"Node `{node_in}` not found in `{model_in}`")
 
     # Infer the shapes of each node.
     #  inferred_model = onnx.shape_inference.infer_shapes(model)
@@ -40,17 +41,18 @@ def add_output_node(
     output_shape = [try_parse_int(x) for x in output_shape]
 
     identity_node = onnx.helper.make_node(
-        'Identity',
-        inputs=[new_output.output[0]],
-        outputs=[node_out]
+        node_type, inputs=[new_output.output[0]], outputs=[node_out]
     )
     model.graph.node.append(identity_node)
 
     output_value_info = onnx.helper.make_tensor_value_info(
         node_out,
-        onnx.TensorProto.FLOAT, shape=output_shape,
+        onnx.TensorProto.FLOAT,
+        shape=output_shape,
     )
     model.graph.output.append(output_value_info)
+
+    onnx.checker.check_model(model)
 
     onnx.save(model, model_out)
 
@@ -58,11 +60,13 @@ def add_output_node(
 if __name__ == "__main__":
     parser = ArgumentParser(
         "Adds a new output `node_out` pointing to a given `input_node` through an Identity node. "
-        "We recommend using a tool such as Netron to check the architecture and node name on `model_in`")
-    parser.add_argument('model_in')
-    parser.add_argument('model_out')
-    parser.add_argument('node_in')
-    parser.add_argument('node_out')
-    parser.add_argument('--output-shape', nargs='+', type=str)
+        "We recommend using a tool such as Netron to check the architecture and node name on `model_in`"
+    )
+    parser.add_argument("model_in")
+    parser.add_argument("model_out")
+    parser.add_argument("node_in")
+    parser.add_argument("node_out")
+    parser.add_argument("--node-type", default="Identity")
+    parser.add_argument("--output-shape", nargs="+", type=str)
 
     add_output_node(**vars(parser.parse_args()))
